962de39b779d8962e3b090878ea25ac8f32cd1d3
KYG17 <sjkyg17@gmail.com>
2024-11-26 12:30:01 +0900

    [kyg] 최초 commit
    	- 표준출력
    	- 이스케이프 문자

---
+++ lec01_basic/src/com/gn/practice/Introduce.java
@@ -0,0 +1,26 @@
+package com.gn.practice;
+
+public class Introduce {
+	public static void main(String[] args) {
+		
+		//안녕하세요~
+		//저는 "김철수"입니다.
+		//나이는 20살이고, mbti는 istp입니다.
+		
+		System.out.print("안녕하세요~\n");
+		System.out.println("저는 \"김철수\" 입니다.");
+		System.out.printf("나이는 %d살이고, mbti는 %s입니다\n", 20, "istp");
+		
+		System.out.print("안녕하세요~\n");
+		System.out.print("저는 \"김철수\"입니다.\n");
+		System.out.print("나이는 20살이고, mbti는 istp입니다\n");
+		
+		System.out.println("안녕하세요");
+		System.out.println("저는 \"김철수\"입니다.");
+		System.out.println("나이는 20살이고, mbti는 istp입니다");
+		
+		System.out.printf("안녕하세요");
+		System.out.printf("저는 \"%s\"입니다., 20");
+				
+	}
+}
