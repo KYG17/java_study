04f5b3e3fb2af8de6f4284dd4b83a96be88a4ec5
KYG17 <sjkyg17@gmail.com>
2024-11-27 14:26:39 +0900

    [kyg] Scanner (nextLine 비교)

---
+++ lec02_variable/src/com/gn/practice/Practice03.java
@@ -0,0 +1,15 @@
+package com.gn.practice;
+
+public class Practice03 {
+
+	public static void main(String[] args) {
+		String word = "제갈민";
+		char first = word.charAt(2);
+		System.out.println(first + "이에요!!");
+		System.out.println(word.charAt(2) + "이에요!!"); 
+		
+	
+
+	}
+
+}
---
+++ lec02_variable/src/com/gn/practice/Practice04.java
@@ -0,0 +1,23 @@
+package com.gn.practice;
+
+public class Practice04 {
+
+	public static void main(String[] args) {
+
+//		아래의 변수들을 선언, 초기화하고 두 수를 더한 값을 아래와 같이 출력하는 프로그램을 구성하세요.
+//		정수형 변수 num1에 10 할당
+//		실수형 변수 num2에 5.7 할당
+//		2) 출력 예시
+//		정수 : 15
+//		실수 : 15.7
+		
+		int num1 = 10;
+		double num2 = 5.7;
+		
+		System.out.println("정수 :"+(num1+(int)num2));
+		System.out.println("실수 :"+(num1 + num2));
+	
+
+	}
+
+}
