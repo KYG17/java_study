371905b4ba3d61e469a9ee855ada475794e4b30a
KYG17 <sjkyg17@gmail.com>
2024-12-02 17:52:10 +0900

    [kyg] 과제

--- lec04_control/src/com/gn/homework/loop/Practice.java
+++ lec04_control/src/com/gn/homework/loop/Practice.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 public class Practice {
 	public void practice01() {
 		System.out.print("숫자 : ");
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		
 	}
 
 }
